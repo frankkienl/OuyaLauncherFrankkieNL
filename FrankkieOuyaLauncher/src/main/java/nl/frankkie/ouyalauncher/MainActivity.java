caf98096347da3e805bb0e7106c4c50cb86f8782
frankkienl <frankkie12345@gmail.com>
2013-07-02 21:43:22 +0200

    First working version

--- FrankkieOuyaLauncher/src/main/java/nl/frankkie/ouyalauncher/MainActivity.java
+++ FrankkieOuyaLauncher/src/main/java/nl/frankkie/ouyalauncher/MainActivity.java
@@ -19,9 +19,9 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
-    GridView gridView;
-//    ListView gridView;
-    MyAdapter adapter;
+    LinearLayout table;
+    //    ListView gridView;
+    //MyAdapter adapter;
     private static ArrayList<ApplicationInfo> mApplications = new ArrayList<ApplicationInfo>();
 
     @Override
@@ -29,19 +29,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         initUI();
         loadApplications();
-        gridView.setSelection(0);
+        fillTable();
     }
 
     protected void initUI() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-        gridView = (GridView) findViewById(R.id.grid);
-//        gridView = (ListView) findViewById(R.id.grid);
-        adapter = new MyAdapter();
-
-        gridView.setAdapter(adapter);
+        setContentView(R.layout.main);
+        table = (LinearLayout) findViewById(R.id.table);
     }
 
 
@@ -79,102 +75,95 @@ public class MainActivity extends Activity {
         }
     }
 
-
-    public class MyAdapter extends BaseAdapter {
-
-
-        @Override
-        public int getCount() {
-            return mApplications.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return mApplications.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-
-        private Rect mOldBounds = new Rect();
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            LinearLayout layout = (LinearLayout) view;
-            LayoutInflater inflater = getLayoutInflater();
-            if (layout == null) {
-                layout = (LinearLayout) inflater.inflate(R.layout.grid_item, gridView, false);
+    public void fillTable() {
+        ViewGroup row = null;
+        LayoutInflater inflater = getLayoutInflater();
+        for (int i = 0; i < mApplications.size(); i++) {
+            if (i % 4 == 0) {
+                if (row != null) {table.addView(row);}
+                row = (ViewGroup) inflater.inflate(R.layout.table_row, table, false);
             }
-            final ApplicationInfo info = (ApplicationInfo) getItem(i);
-            ////////
-            ////////
+            View v = fillTable(i);
+            row.addView(v);
+        }
+    }
 
-            Drawable icon = info.icon;
+    private Rect mOldBounds = new Rect();
 
-            if (!info.filtered) {
-                //final Resources resources = getContext().getResources();
-                int width = 180;//(int) resources.getDimension(android.R.dimen.app_icon_size);
-                int height = 180;//(int) resources.getDimension(android.R.dimen.app_icon_size);
+    public Object getItem(int i) {
+        return mApplications.get(i);
+    }
 
-                final int iconWidth = icon.getIntrinsicWidth();
-                final int iconHeight = icon.getIntrinsicHeight();
+    public View fillTable(int id) {
+        LinearLayout layout;
+        LayoutInflater inflater = getLayoutInflater();
+        layout = (LinearLayout) inflater.inflate(R.layout.grid_item, table, false);
+        final ApplicationInfo info = (ApplicationInfo) getItem(id);
+        ////////
+        ////////
 
-                if (icon instanceof PaintDrawable) {
-                    PaintDrawable painter = (PaintDrawable) icon;
-                    painter.setIntrinsicWidth(width);
-                    painter.setIntrinsicHeight(height);
+        Drawable icon = info.icon;
+
+        if (!info.filtered) {
+            //final Resources resources = getContext().getResources();
+            int width = 180;//(int) resources.getDimension(android.R.dimen.app_icon_size);
+            int height = 180;//(int) resources.getDimension(android.R.dimen.app_icon_size);
+
+            final int iconWidth = icon.getIntrinsicWidth();
+            final int iconHeight = icon.getIntrinsicHeight();
+
+            if (icon instanceof PaintDrawable) {
+                PaintDrawable painter = (PaintDrawable) icon;
+                painter.setIntrinsicWidth(width);
+                painter.setIntrinsicHeight(height);
+            }
+
+            if (width > 0 && height > 0 && (width < iconWidth || height < iconHeight)) {
+                final float ratio = (float) iconWidth / iconHeight;
+
+                if (iconWidth > iconHeight) {
+                    height = (int) (width / ratio);
+                } else if (iconHeight > iconWidth) {
+                    width = (int) (height * ratio);
                 }
 
-                if (width > 0 && height > 0 && (width < iconWidth || height < iconHeight)) {
-                    final float ratio = (float) iconWidth / iconHeight;
-
-                    if (iconWidth > iconHeight) {
-                        height = (int) (width / ratio);
-                    } else if (iconHeight > iconWidth) {
-                        width = (int) (height * ratio);
-                    }
-
-                    final Bitmap.Config c =
-                            icon.getOpacity() != PixelFormat.OPAQUE ?
-                                    Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565;
-                    final Bitmap thumb = Bitmap.createBitmap(width, height, c);
-                    final Canvas canvas = new Canvas(thumb);
-                    canvas.setDrawFilter(new PaintFlagsDrawFilter(Paint.DITHER_FLAG, 0));
-                    // Copy the old bounds to restore them later
-                    // If we were to do oldBounds = icon.getBounds(),
-                    // the call to setBounds() that follows would
-                    // change the same instance and we would lose the
-                    // old bounds
-                    mOldBounds.set(icon.getBounds());
-                    icon.setBounds(0, 0, width, height);
-                    icon.draw(canvas);
-                    icon.setBounds(mOldBounds);
-                    icon = info.icon = new BitmapDrawable(thumb);
-                    info.filtered = true;
+                final Bitmap.Config c =
+                        icon.getOpacity() != PixelFormat.OPAQUE ?
+                                Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565;
+                final Bitmap thumb = Bitmap.createBitmap(width, height, c);
+                final Canvas canvas = new Canvas(thumb);
+                canvas.setDrawFilter(new PaintFlagsDrawFilter(Paint.DITHER_FLAG, 0));
+                // Copy the old bounds to restore them later
+                // If we were to do oldBounds = icon.getBounds(),
+                // the call to setBounds() that follows would
+                // change the same instance and we would lose the
+                // old bounds
+                mOldBounds.set(icon.getBounds());
+                icon.setBounds(0, 0, width, height);
+                icon.draw(canvas);
+                icon.setBounds(mOldBounds);
+                icon = info.icon = new BitmapDrawable(thumb);
+                info.filtered = true;
+            }
+        }
+        ////////
+        ////////
+        TextView tv = (TextView) layout.findViewById(R.id.item_text);
+        tv.setText(info.title);
+        ImageView img = (ImageView) layout.findViewById(R.id.item_image);
+        img.setImageDrawable(info.icon);
+        ///
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = info.intent;
+                try {
+                    startActivity(i);
+                } catch (Exception e) {
                 }
             }
-            ////////
-            ////////
-            TextView tv = (TextView) layout.findViewById(R.id.item_text);
-            tv.setText(info.title);
-            ImageView img = (ImageView) layout.findViewById(R.id.item_image);
-            img.setImageDrawable(info.icon);
-            ///
-            layout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent i = info.intent;
-                    try {
-                        startActivity(i);
-                    } catch (Exception e) {
-                    }
-                }
-            });
-            return layout;
-        }
+        });
+        return layout;
     }
 
 }
